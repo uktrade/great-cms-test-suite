b0d9eb01d4e4398f1562c475c372f7afc75191e4
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2023-12-15 08:32:44 +0000

    FIXED Flake8 errors.

--- tests/behavioural/features/steps/tutorial.py
+++ tests/behavioural/features/steps/tutorial.py
@@ -1,16 +1,16 @@
-from behave import *
+from behave import given, when, then
 
 
 @given('we have behave installed')
-def step_impl(context):
+def step_install(context):
     pass
 
 
 @when('we implement a test')
-def step_impl(context):
+def step_implement_test(context):
     assert True is not False
 
 
 @then('behave will test it for us!')
-def step_impl(context):
+def step_test(context):
     assert context.failed is False
