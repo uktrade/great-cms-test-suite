6fd4e6b1c302d490faf5225fa0cf9366414222a1
bobby-didcoding <bobby@didcoding.com>
2024-03-22 10:36:11 +0000

    Added signout tests

--- tests/behavioural/features/sso.feature
+++ tests/behavioural/features/sso.feature
@@ -17,3 +17,11 @@ Feature: Single Sign On
     Given I am on the great homepage
     When I complete the sign in steps
     Then I am taken to the user dashboard page
+
+  @Chrome
+  Scenario: User can sign out to the Great services
+    Given I am on the main homepage
+    When I complete the authentication steps
+    When I am taken to the dashboard page
+    When I click the sign out button
+    Then I am taken back to great.gov.uk
---
+++ tests/behavioural/features/steps/sso_sign_out.py
@@ -0,0 +1,42 @@
+from behave import given, then, when
+
+from tests.behavioural.pages.homepage import HomePage
+from tests.behavioural.pages.sso.dashboard_page import DashboardPage
+
+
+@given('I am on the main homepage')
+def step_start_on_great(context):
+    homepage = HomePage(context)
+    homepage.get_url()
+    homepage.accept_domestic_cookies()
+    assert context.browser.current_url == homepage.url
+
+
+@when('I complete the authentication steps')
+def step_sign_in(context):
+    context.execute_steps(
+        """
+            When I click the homepage sign in link
+            And I enter sso login credentials
+        """
+    )
+
+
+@when('I am taken to the dashboard page')
+def step_user_dashboard_page(context):
+    dashboard_page = DashboardPage(context)
+    assert dashboard_page.url in context.browser.current_url
+
+
+@when('I click the sign out button')
+def step_user_sign_out(context):
+    dashboard_page = DashboardPage(context)
+    dashboard_page.press_menu_link()
+    dashboard_page.press_sign_out_link()
+
+
+@then('I am taken back to great.gov.uk')
+def step_user_home_page(context):
+    homepage = HomePage(context)
+    homepage.get_url()
+    assert context.browser.current_url == homepage.url
